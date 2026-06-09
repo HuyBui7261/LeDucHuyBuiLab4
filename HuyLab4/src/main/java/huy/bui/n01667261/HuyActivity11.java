6f871099baa9db9ec4e46892f7dbc7312c8fa7ae
HuyBui7261 <huyb85mmiv@gmail.com>
2026-06-09 15:24:01 -0400

    4td commit and push
    adding imgbutton a number only text edit
    function when user input number in the edit text and push the imgbutton it will send the input to the second activity there user does not put anything in edit text it will launch second screen but no data pass and before pass a snackbar will be displayed with a 'start' confirm button

--- HuyLab4/src/main/java/huy/bui/n01667261/HuyActivity11.java
+++ HuyLab4/src/main/java/huy/bui/n01667261/HuyActivity11.java
@@ -1,24 +1,48 @@
 package huy.bui.n01667261;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class HuyActivity11 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        EditText editTextDigits = findViewById(R.id.editTextText);
+        ImageButton imageButtonSubmit = findViewById(R.id.imageButton);
+
+
+        imageButtonSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Your Full Name", Snackbar.LENGTH_INDEFINITE)
+                        .setAction("START", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+
+                                String inputText = editTextDigits.getText().toString().trim();
+                                Intent intent = new Intent(HuyActivity11.this, BuiActivity22.class);
+                                if (!inputText.isEmpty()) {
+                                    try {
+                                        long dataAsLong = Long.parseLong(inputText);
+                                        intent.putExtra("EXTRA_NUMBER_DATA", dataAsLong);
+                                    } catch (NumberFormatException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                                editTextDigits.setText("");
+                                startActivity(intent);
+                            }
+                        }).show();
+            }
         });
     }
 }
